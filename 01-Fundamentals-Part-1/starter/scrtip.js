81965d2b824a7980d2b503b9f84949ee239ff1bf
haval95 <havalrezan22@gmail.com>
2024-07-16 00:34:07 +0200

    vars and operators

---
+++ 01-Fundamentals-Part-1/starter/scrtip.js
@@ -0,0 +1,38 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('js amazing!');
+// console.log(10 + 10 + 23);
+
+// let $js = 'haval';
+
+// console.log($js);
+
+let x = true;
+// console.log(typeof x);
+// console.log(typeof 'hello');
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1995;
+// console.log(year);
+// console.log(typeof year);
+// // error in js
+// console.log(typeof null);
+
+let b = 'haval';
+const a = 'haval';
+
+console.log(b, a);
+console.log(typeof a, typeof b);
+
+b = 1;
+
+console.log(b, a);
+var c = 10;
+console.log(c);
+
+let z, y;
+
+z = y = 10;
+console.log(z, y);
